df988e99494ac02d8a8c65f1f4fc63e3510412c6
hamony1029 <chltpwls1029@gmail.com>
2025-06-10 13:10:15 +0900

    search 함수 수정, 함수 return query로 변경

--- app.py
+++ app.py
@@ -4,26 +4,27 @@ from datetime import datetime
 
 app = Flask(__name__)
 
+
 @app.route('/movies_page')
 def movies_page():
     return render_template('movies.html')
 
-#영화명컴색(일부)
+
+# 영화명컴색(일부)
 @app.route('/movies/title_search_result',
            methods=['GET'])
 def search_movies_by_title():
-    title=request.args.get('title','')
-
-    conn, cur=open_db()
-
-    query ="""
+    title = request.args.get('title', '')
+    
+    conn, cur = open_db()
+    
+    query = """
     select * from Movie_info where title_kr like %s
     """
-    cur.execute(query,(f'%{title}%',))
-    result =cur.fetchall()
-    close_db(conn,cur)
+    cur.execute(query, (f'%{title}%',))
+    result = cur.fetchall()
+    close_db(conn, cur)
     return jsonify(result)
-
 
 
 # 감독명
@@ -31,16 +32,13 @@ def search_movies_by_title():
            methods=['GET'])
 def get_movies_by_director():
     director = request.args.get('director')
-    conn, cur = open_db()
-    query = """
-            select *
-            from Movie_info
-            where (slecet mid from movie_director
-                    where (select did from director where director like %s)"""
-    cur.execute(query, (f'%{director}%',))
-    result = cur.fetchall()
-    close_db(conn, cur)
-    return jsonify(result)
+    # conn, cur = open_db()
+    query = (f" (select mid from movie_director\n"
+             f"     where (select did from director where director like {director})")
+    # cur.execute(query, (f'%{director}%',))
+    # result = cur.fetchall()
+    # close_db(conn, cur)
+    return query
 
 
 # 제작연도
@@ -51,35 +49,27 @@ def get_movies_by_year():
     y2 = request.args.get('year2')
     if y2 is None:
         y2 = datetime.today().year
-    conn, cur = open_db()
-    query = """
-        select *
-        from Movie_info
-        where year >= %s and year <= %s"""
-    cur.execute(query, (f'%{y1}%', f'%{y2}%',))
-    result = cur.fetchall()
-    close_db(conn, cur)
-    return jsonify(result)
+    # conn, cur = open_db()
+    query = f"where year >= {y1} and year <= {y2}"
+    # cur.execute(query, (f'%{y1}%', f'%{y2}%',))
+    # result = cur.fetchall()
+    # close_db(conn, cur)
+    return query
 
 
 # 제작상태
 @app.route('/movies/status/<status>',
            methods=['GET'])
 def get_movies_by_status(status):
-    conn, cur = open_db()
+    # conn, cur = open_db()
     status_list = [t.strip() for t in status.split(',') if t.strip()]
     # 동적 IN 절
     placeholders = ','.join(['%s'] * len(status_list))
-    query = f"""
-    SELECT *
-    FROM Movie_info
-    WHERE status IN ({placeholders})
-    """
-    cur.execute(query, status_list)
-    result = cur.fetchall()
-    
-    close_db(conn, cur)
-    return jsonify(result)
+    query = f"status IN ({placeholders})\n"
+    # cur.execute(query, status_list)
+    # result = cur.fetchall()
+    # close_db(conn, cur)
+    return query
 
 
 # 제작상태
@@ -87,82 +77,73 @@ def get_movies_by_status(status):
            methods=['GET'])
 def get_movies_by_type(type_):
     conn, cur = open_db()
-    query = """
-    select *
-    from Movie_info
-    where type = %s
-    """
     type_list = [t.strip() for t in type_.split(',') if t.strip()]
     # 동적 IN 절
     placeholders = ','.join(['%s'] * len(type_list))
-    query = f"""
-    SELECT *
-    FROM Movie_info
-    WHERE type IN ({placeholders})
-    """
-    cur.execute(query, type_list)
-    result = cur.fetchall()
-    
-    close_db(conn, cur)
-    return jsonify(result)
+    query = f"WHERE type IN ({placeholders})"
+    # cur.execute(query, type_list)
+    # result = cur.fetchall()
+    # close_db(conn, cur)
+    return query
 
 
-#장르별
+# 장르별
 @app.route('/movies/genre/<genres>',
            methods=['GET'])
 def get_movies_by_genre(genres):
-    genre_list=[g.strip()for g in genres.split(',')if g.strip()]
-
-    conn,cur=open_db()
-
-    placeholders=','.join(['%s'] * len(genre_list))
-
-    query=f"""
+    genre_list = [g.strip() for g in genres.split(',') if g.strip()]
+    
+    conn, cur = open_db()
+    
+    placeholders = ','.join(['%s'] * len(genre_list))
+    
+    query = f"""
     select distinct m.* 
     from Movie_info m 
     join Movie_Genre mg on m.movie_id=mg.movie_id
     join Genre g on g.genre_id=mg.genre_id
     where g.genre in ({placeholders})
     """
-    cur.execute(query,genre_list)
-    result=cur.fetchall()
-    close_db(conn,cur)
+    cur.execute(query, genre_list)
+    result = cur.fetchall()
+    close_db(conn, cur)
     return jsonify(result)
 
 
-#국적별
+# 국적별
 @app.route('/movies/country/<countries>',
            methods=['GET'])
 def get_movies_by_country(countries):
-    country_list=[c.strip()for c in countries.split(',')if c.strip()]
-
-    conn,cur=open_db()
-
-    placeholders=','.join(['%s'] * len(country_list))
-
-    query=f"""
+    country_list = [c.strip() for c in countries.split(',') if c.strip()]
+    
+    conn, cur = open_db()
+    
+    placeholders = ','.join(['%s'] * len(country_list))
+    
+    query = f"""
     select distinct m.* 
     from Movie_info m
     join Movie_country mc on m.movie_id=mc.movie_id
     join Country c on c.country_id= mc.country_id
     where c.country in ({placeholders})
     """
-    cur.execute(query,country_list)
-    result=cur.fetchall()
-    close_db(conn,cur)
+    cur.execute(query, country_list)
+    result = cur.fetchall()
+    close_db(conn, cur)
     return jsonify(result)
 
-#등급별 - 성인 장르일 경우 19세 이상
+
+# 등급별 - 성인 장르일 경우 19세 이상
 @app.route('/movies/rating/<ratings>',
            methods=['GET'])
 def get_movie_by_rating(ratings):
-    rating_list=[r.strip()for r in ratings.split(',')if r.strip()]
-
-    conn,cur=open_db()
-
+    rating_list = [r.strip() for r in ratings.split(',') if r.strip()]
+    
+    conn, cur = open_db()
+    
     if "19세 이상" in rating_list:
- #'성인' 장르인 영화만 필터링
-        query="""
+        # '성인' 장르인 영화만 필터링
+        query = """
         select m.* 
         from Movie_info m
         join Movie_Genre mg on m.movie_id=mg.movie_id
@@ -171,27 +152,27 @@ def get_movie_by_rating(ratings):
         """
         cur.execute(query)
     else:
-        placeholders=','.join(['%s'] * len(rating_list))
-
-        query=f"select * from Movie_info where rating in({placeholders})"
-        cur.execute(query,rating_list)
-
-    result=cur.fetchall()
-    close_db(conn,cur)
+        placeholders = ','.join(['%s'] * len(rating_list))
+        
+        query = f"select * from Movie_info where rating in({placeholders})"
+        cur.execute(query, rating_list)
+    
+    result = cur.fetchall()
+    close_db(conn, cur)
     return jsonify(result)
 
 
-#대표국적별(대표국적=처음으로 등록된 국적)
+# 대표국적별(대표국적=처음으로 등록된 국적)
 @app.route('/movies/primary_country/<countries>',
            methods=['GET'])
 def get_movies_by_primary_country(countries):
-    country_list=[c.strip()for c in countries.split(',')if c.strip()]
-
-    conn, cur=open_db()
-
-    placeholders=','.join(['%s'] * len(country_list))
-
-    query=f"""
+    country_list = [c.strip() for c in countries.split(',') if c.strip()]
+    
+    conn, cur = open_db()
+    
+    placeholders = ','.join(['%s'] * len(country_list))
+    
+    query = f"""
     select m.*
     from Movie_info m
     join(
@@ -203,20 +184,19 @@ def get_movies_by_primary_country(countries):
     join Country c on mc.primary_country_id=c.country_id
     where c.country in ({placeholders})
     """
-    cur.execute(query,country_list)
-    result=cur.fetchall()
-    close_db(conn,cur)
+    cur.execute(query, country_list)
+    result = cur.fetchall()
+    close_db(conn, cur)
     return jsonify(result)
 
 
-#인덱싱
+# 인덱싱
 @app.route('/movies/title_index/<index>',
-            methods=['GET'])
+           methods=['GET'])
 def get_movies_by_title_index(index):
-
     conn, cur = open_db()
     
-# 초성에 따른 한글 시작/끝 범위
+    # 초성에 따른 한글 시작/끝 범위
     chosung_range = {
         'ㄱ': ('가', '나'),
         'ㄴ': ('나', '다'),
@@ -231,9 +211,9 @@ def get_movies_by_title_index(index):
         'ㅋ': ('카', '타'),
         'ㅌ': ('타', '파'),
         'ㅍ': ('파', '하'),
-        'ㅎ': ('하', '힣')  
+        'ㅎ': ('하', '힣')
     }
-
+    
     if index in chosung_range:
         start, end = chosung_range[index]
         query = """
@@ -247,27 +227,10 @@ def get_movies_by_title_index(index):
         cur.execute(query, (index.upper() + '%',))
     else:
         return jsonify([])
-
+    
     result = cur.fetchall()
     close_db(conn, cur)
     return jsonify(result)
-
-
-# # 제작연도순 정렬
-# def order_by_year():
-#     return " order by year desc"
-#
-#
-# # 영화명순 정렬
-# def order_by_title():
-#     return """
-#         order by
-#         CASE
-#             WHEN title_kr IS NULL OR title_kr = '' THEN 1 ELSE 0
-#         END,
-#         title_kr,
-#         title_en
-#     """
 
 
 # 조회
@@ -278,7 +241,7 @@ def search():
     # 각 조회기능별 함수는 get한 값을 "where ~" 형식으로 조건만 담아 return
     # 각 변수에 return된 값을 담아 값이 있으면 search_sql에 join
     query = "select * from movie_info"
-    condition = [search_movies(),
+    condition = [search_movies_by_title(),
                  get_movies_by_director(),
                  get_movies_by_year(),
                  get_movies_by_status(),
@@ -288,11 +251,15 @@ def search():
                  get_movie_by_rating(),
                  get_movies_by_primary_country(),
                  get_movies_by_title_index(),
-                ]
+                 ]
     
     for i in condition:
-        query += " " + condition[i]
-
+        if condition[i] == "":
+            continue
+        if i == 0:
+            query += " where " + condition[i]
+        query += " and " + condition[i]
+    
     conn, cur = open_db()
     cur.execute(query)
     result = cur.fetchall()
@@ -300,16 +267,18 @@ def search():
     close_db(conn, cur)
     return jsonify(result)
 
+
 # 초기화, 정렬은 js로만 가능
 @app.route('/movies',
            methods=['GET'])
 def get_all_movies():
-    conn,cur=open_db()
-    query="select * from Movie_info"
+    conn, cur = open_db()
+    query = "select * from Movie_info"
     cur.execute(query)
-    result=cur.fetchall()
-    close_db(conn,cur)
+    result = cur.fetchall()
+    close_db(conn, cur)
     return jsonify(result)
+
 
 # Flask
 if __name__ == '__main__':
